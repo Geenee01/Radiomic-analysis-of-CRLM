8c6e7890371ab9ddf22bc448c7c1006b89cf3fc0
Hemangini Patel <135769142+Geenee01@users.noreply.github.com>
2024-07-29 18:34:23 -0400

    Update mRMR_feature_selection.R

--- mRMR_feature_selection.R
+++ mRMR_feature_selection.R
@@ -1,12 +1,12 @@
-# mrmr feature selection for all aggregation files for overall survival
+# mrmr feature selection 
 
 #Load necessary libraries
 library(mRMRe)
 
 ### UWA ###
-survdata <- readxl::read_xlsx("C:/Users/Hemangini/Desktop/MCRCdata/Analysis0-300/CRLM Clinical data.xlsx",col_names = TRUE) [,-c(1)]
-surv_object <- Surv(event = survdata$vital_status, time = survdata$overall_survival_months)
-mydata <- read.table("C:/Users/Hemangini/Desktop/MCRCdata/Analysis0-300/UWA_radiomic_aggregation.csv", header=TRUE, sep=",")[,-c(1)]
+survdata <- readxl::read_xlsx("path/to/clinical.xlsx",col_names = TRUE) [,-c(1)]
+surv_object <- Surv(event = survdata$fu status, time = survdata$overall surv months)
+mydata <- read.table("path/to/UWA_radiomic_aggregation.csv", header=TRUE, sep=",")[,-c(1)]
 newdata <- cbind(surv_object, mydata)
 #print(newdata)
 dd <- mRMR.data(data = newdata)
@@ -19,13 +19,13 @@ features <- featureNames(filter) # all feature with indices used in mrmr
 solutions_indices <- solutions(filter) # list of feature indices
 solutions_indices <- unlist(solutions_indices) # converting list into vector
 selected_features <- features[solutions_indices] # getting corresponding feature names
-write.csv(selected_features, "C:/Users/Hemangini/Desktop/MCRCdata/Analysis0-300/mrmr_overall_survival/UWA_10features_mRMR.csv", row.names = FALSE)
+write.csv(selected_features, "path/to/UWA_10features_mRMR.csv", row.names = FALSE)
 
 
 ### WA ###
-survdata <- readxl::read_xlsx("C:/Users/Hemangini/Desktop/MCRCdata/Analysis0-300/CRLM Clinical data.xlsx",col_names = TRUE)
-surv_object <- Surv(event = survdata$vital_status, time = survdata$overall_survival_months)
-mydata <- read.table("C:/Users/Hemangini/Desktop/MCRCdata/Analysis0-300/WA_radiomic_aggregation.csv", header=TRUE, sep=",")[,-c(1)]
+survdata <- readxl::read_xlsx("path/to/clinical.xlsx",col_names = TRUE)
+surv_object <- Surv(event = survdata$fu status, time = survdata$overall surv months)
+mydata <- read.table("path/to/WA_radiomic_aggregation.csv", header=TRUE, sep=",")[,-c(1)]
 newdata <- cbind(surv_object, mydata)
 dd <- mRMR.data(data = newdata)
 filter <- mRMR.classic("mRMRe.Filter", data = dd, target_indices = c(1), feature_count = 10)
@@ -33,12 +33,12 @@ features <- featureNames(filter) # all feature with indices used in mrmr
 solutions_indices <- solutions(filter)
 solutions_indices <- unlist(solutions_indices)
 selected_features <- features[solutions_indices]
-write.csv(selected_features, "C:/Users/Hemangini/Desktop/MCRCdata/Analysis0-300/mrmr_overall_survival/WA_10features_mRMR.csv", row.names = FALSE)
+write.csv(selected_features, "path/to/WA_10features_mRMR.csv", row.names = FALSE)
 
 ### WA of largest 3 ###
-survdata <- readxl::read_xlsx("C:/Users/Hemangini/Desktop/MCRCdata/Analysis0-300/CRLM Clinical data.xlsx",col_names = TRUE)
-surv_object <- Surv(event = survdata$vital_status, time = survdata$overall_survival_months)
-mydata <- read.table("C:/Users/Hemangini/Desktop/MCRCdata/Analysis0-300/largest3_radiomic_aggregation.csv", header=TRUE, sep=",")[,-c(1)]
+survdata <- readxl::read_xlsx("path/to/clinical.xlsx",col_names = TRUE)
+surv_object <- Surv(event = survdata$fu status, time = survdata$overall surv months)
+mydata <- read.table("path/to/largest3_aggregation.csv", header=TRUE, sep=",")[,-c(1)]
 newdata <- cbind(surv_object, mydata)
 dd <- mRMR.data(data = newdata)
 filter <- mRMR.classic("mRMRe.Filter", data = dd, target_indices = c(1), feature_count = 10)
@@ -46,12 +46,12 @@ features <- featureNames(filter) # all feature with indices used in mrmr
 solutions_indices <- solutions(filter)
 solutions_indices <- unlist(solutions_indices)
 selected_features <- features[solutions_indices]
-write.csv(selected_features, 'C:/Users/Hemangini/Desktop/MCRCdata/Analysis0-300/mrmr_overall_survival/largest3_10features_mRMR.csv', row.names = FALSE)
+write.csv(selected_features, 'path/to/largest3_10features_mRMR.csv', row.names = FALSE)
 
 ### Largest 1 ###
-survdata <- readxl::read_xlsx("C:/Users/Hemangini/Desktop/MCRCdata/Analysis0-300/CRLM Clinical data.xlsx",col_names = TRUE)
-surv_object <- Surv(event = survdata$vital_status, time = survdata$overall_survival_months)
-mydata <- read.table("C:/Users/Hemangini/Desktop/MCRCdata/Analysis0-300/largest1_radiomic.csv", header=TRUE, sep=",")[,-c(1)]
+survdata <- readxl::read_xlsx("path/to/clinical.xlsx",col_names = TRUE)
+surv_object <- Surv(event = survdata$fu status, time = survdata$overall surv months)
+mydata <- read.table("path/to/largest1_radiomic.csv", header=TRUE, sep=",")[,-c(1)]
 mydata <- mydata[,-ncol(mydata)]
 newdata <- cbind(surv_object, mydata)
 dd <- mRMR.data(data = newdata)
@@ -60,12 +60,12 @@ features <- featureNames(filter) # all feature with indices used in mrmr
 solutions_indices <- solutions(filter)
 solutions_indices <- unlist(solutions_indices)
 selected_features <- features[solutions_indices]
-write.csv(selected_features, 'C:/Users/Hemangini/Desktop/MCRCdata/Analysis0-300/mrmr_overall_survival/largest1_10features_mRMR.csv', row.names = FALSE)
+write.csv(selected_features, 'path/to/largest1_10features_mRMR.csv', row.names = FALSE)
 
 ### Smallest 1 ###
-survdata <- readxl::read_xlsx("C:/Users/Hemangini/Desktop/MCRCdata/Analysis0-300/CRLM Clinical data.xlsx",col_names = TRUE)
-surv_object <- Surv(event = survdata$vital_status, time = survdata$overall_survival_months)
-mydata <- read.table("C:/Users/Hemangini/Desktop/MCRCdata/Analysis0-300/smallest1_radiomic.csv", header=TRUE, sep=",")[,-c(1)]
+survdata <- readxl::read_xlsx("path/to/clinical.xlsx",col_names = TRUE)
+surv_object <- Surv(event = survdata$fu status, time = survdata$overall surv months)
+mydata <- read.table("path/to/smallest1_radiomic.csv", header=TRUE, sep=",")[,-c(1)]
 newdata <- cbind(surv_object, mydata)
 dd <- mRMR.data(data = newdata)
 filter <- mRMR.classic("mRMRe.Filter", data = dd, target_indices = c(1), feature_count = 10)
@@ -73,4 +73,4 @@ features <- featureNames(filter) # all feature with indices used in mrmr
 solutions_indices <- solutions(filter)
 solutions_indices <- unlist(solutions_indices)
 selected_features <- features[solutions_indices]
-write.csv(selected_features, 'C:/Users/Hemangini/Desktop/MCRCdata/Analysis0-300/mrmr_overall_survival/smallest1_10features_mRMR.csv', row.names = FALSE)
+write.csv(selected_features, 'path/to/smallest1_10features_mRMR.csv', row.names = FALSE)
